d08aef2b3380c89b154db9a9b1d213505b496375
Adam3192 <jeremyadixon@hotmail.com>
2022-09-23 16:27:03 -0500

    good copy, worked on search feature

--- frontEnd/src/components/SearchUsers.js
+++ frontEnd/src/components/SearchUsers.js
@@ -1,34 +1,34 @@
 import React, { useContext, useState, useEffect } from 'react'
 import Button from 'react-bootstrap/Button'
-import { useNavigate, useParams, Link } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 import UserContext from '../contexts/UserContext'
 
 const SearchUsers = () => {
   let navigate = useNavigate()
   let { searchUsers } = useContext(UserContext)
-  let [users, setUsers] = useState([])
+  let [users, setUsers] = useState(null)
   let [search, setSearch] = useState('')
-  let params = useParams()
+  const test = users
 
   useEffect(() => {
-   async function getUser() {
-     await searchUsers(search).then((response) => {
-       setUsers(response)
-     })
-   }
+    async function getUser() {
+      await searchUsers(search).then((response) => {
+        setUsers(response)
+      })
+    }
 
-   getUser()
- }, [search])
+    getUser()
+  }, [search])
 
   const handleChange = (e) => {
-   if (e.target.value === '') return
-   e.preventDefault()
-   setSearch(e.target.value)
- }
+    if (e.target.value === '') return
+    e.preventDefault()
+    setSearch(e.target.value)
+  }
 
   return (
     <div>
-    <h1>Search Users</h1>
+      <h1>Search Users</h1>
       <form>
         <div className="container5">
           <textarea
@@ -37,23 +37,34 @@ const SearchUsers = () => {
             name="message"
             onChange={handleChange}
           />
-          <Button type='submit' className="style2 ml-10"  variant="info">Search</Button>
+          <Button type="submit" className="style2 ml-10" variant="info">
+            Search
+          </Button>
         </div>
       </form>
-      {users.map((u) => {
-                return (
-                  <div className="tweet" key={u._id}>
-                    <div className="container">
-                      <p className="style3">{u.username}</p>
-                      <p className="style3">{`${u.firstName} ${u.lastName}`}</p>
-                      <div>
-                        <Link className='gridItem2' to={`/tweeter/user2/${u.username}`}>View Profile</Link>
-                      </div>
-                    </div>
-                    <br></br>
-                  </div>
-                )
-              })}
+      {(test !== null) ? (
+        users.map((u) => {
+          return (
+            <div className="tweet" key={u._id}>
+              <div className="container">
+                <p className="style3">{u.username}</p>
+                <p className="style3">{`${u.firstName} ${u.lastName}`}</p>
+                <div>
+                  <Link
+                    className="gridItem2"
+                    to={`/tweeter/user2/${u.username}`}
+                  >
+                    View Profile
+                  </Link>
+                </div>
+              </div>
+              <br></br>
+            </div>
+          )
+        })
+      ) : (
+         <p>No matching users found</p>
+      )}
     </div>
   )
 }
